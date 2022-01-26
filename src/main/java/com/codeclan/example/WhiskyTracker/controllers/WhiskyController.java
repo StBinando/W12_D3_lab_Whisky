153f7adbac79957df607a2ba0d5971bf2d232d0a
Stefano Binando <sbinando@gmail.com>
2022-01-26 14:19:04 +0000

    First commit (and final)

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,43 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getWhiskiesByYear(){
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/whiskies/getByYear")
+    public ResponseEntity<List<Whisky>> getWhiskiesByYear(@RequestParam(name = "year") int year){
+        return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/getByRegion")
+    public ResponseEntity<List<Whisky>> getWhiskiesByDistilleryRegion(@RequestParam(name = "region") String region){
+        return new ResponseEntity<>(whiskyRepository.findByDistilleryRegion(region), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/getByAgeAndDistilleryName")
+    public ResponseEntity<List<Whisky>> getWhiskiesByAgeAndDistilleryName(@RequestParam(name = "age") int age, @RequestParam(name = "distilleryname") String distilleryname){
+        return new ResponseEntity<>(whiskyRepository.findByAgeAndDistilleryName(age, distilleryname), HttpStatus.OK);
+    }
+
+}
